90a369d6d56628c34fb4eb2b8d68779ed9d2c611
3.dll <giahanv375@gmail.com>
2026-05-30 03:43:45 -0700

    Update ui.lua

--- YouTube Player Script/ui.lua
+++ YouTube Player Script/ui.lua
@@ -1,1 +1,8 @@
+-- Configuration Settings
+-- w: Screen width (horizontal length)
+-- h: Screen height
+-- sz: Pixel size (scale of the pixels)
+-- Y Slider: Adjust screen vertical position (high/low)
+-- URL Box: YouTube video link input
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/op3r4-eptsien/core/refs/heads/main/93'))()
